62d431903ee816be7d1e2e33058e055d3126065a
cquirrel <9157825+cquirrel@users.noreply.github.com>
2025-08-12 15:24:33 +0100

    Adding destroy script

---
+++ infra/destroy_infra.sh
@@ -0,0 +1,10 @@
+BACKEND_BUCKET_NAME="cquirrel-ec2-docker-tf-backend"
+BACKEND_DYNAMODB_TABLE="terraform-locks"
+
+terraform init \
+  -backend-config="bucket=$BACKEND_BUCKET_NAME" \
+  -backend-config="dynamodb_table=$BACKEND_DYNAMODB_TABLE" \
+  -backend-config="key=terraform.tfstate" \
+  -backend-config="encrypt=true"
+
+terraform apply -auto-approve
