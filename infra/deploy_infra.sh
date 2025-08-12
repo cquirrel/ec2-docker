02f11b823a3191b4017c24ca9d0d00d66e142557
cquirrel <9157825+cquirrel@users.noreply.github.com>
2025-08-12 15:49:03 +0100

    Fixing deploy script

--- infra/deploy_infra.sh
+++ infra/deploy_infra.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$PORTAINER_PASS" ] || [ -z "$NGINX_PM_USER" ] || [ -z "$NGINX_PM_PASS" ]; then
+  echo "Error: One or more required environment variables are not set: PORTAINER_PASS, NGINX_PM_USER, NGINX_PM_PASS"
+  exit 1
+fi
+
 # Backend Variables
 BACKEND_BUCKET_NAME="cquirrel-ec2-docker-tf-backend"
 BACKEND_DYNAMODB_TABLE="terraform-locks"
