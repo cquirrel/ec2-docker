3357bb96cd8579ecdaff355615773006a5f1afc8
cquirrel <9157825+cquirrel@users.noreply.github.com>
2025-08-14 10:20:21 +0100

    Using cloudinit template

--- infra/instance.tf
+++ infra/instance.tf
@@ -1,9 +1,13 @@
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
-  #key_name               = aws_key_pair.deployer.key_name
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  user_data              = file("cloud-init-processed.yml")
+
+  user_data = templatefile("${path.module}/cloud-init.yml", {
+    portainer_password = var.portainer_password,
+    nginx_proxy_manager_email  = var.nginx_proxy_manager_email,
+    nginx_proxy_manager_password  = var.nginx_proxy_manager_password
+  })
 
   tags = {
     Name = "DockerHost"
---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "portainer_password" {
+  description = "Portainer UI Admin Password"
+  type        = string
+}
+
+variable "nginx_proxy_manager_email" {
+  description = "Nginx Proxy Manager UI E-mail Address"
+  type        = string
+}
+
+variable "nginx_proxy_manager_password" {
+  description = "Nginx Proxy Manager UI Password"
+  type        = string
+}
