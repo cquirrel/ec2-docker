220e8890a6fa9e5f7aa53f4d624c6a6489b15078
cquirrel <9157825+cquirrel@users.noreply.github.com>
2025-09-10 12:17:03 +0100

    Cloudflare tunnels testing. NOT WORKING

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,20 +1,10 @@
-variable "portainer_password" {
-  description = "Portainer UI Admin Password"
-  type        = string
-}
-
-variable "nginx_proxy_manager_email" {
-  description = "Nginx Proxy Manager UI E-mail Address"
-  type        = string
-}
-
-variable "nginx_proxy_manager_password" {
-  description = "Nginx Proxy Manager UI Password"
-  type        = string
-}
-
 variable "instance_swap_size" {
   description = "Server instance swap size in gigabytes"
-  type = number
-  default = 4
+  type        = number
+  default     = 4
+}
+
+variable "cf_tunnel_id" {
+  description = "Cloudflare Tunnel ID"
+  type        = string
 }
